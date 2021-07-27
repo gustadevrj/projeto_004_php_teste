7c2e6ab79ee87a52709cd0de530195c4ccfbe4e1
Gustavo Oliveira <55470788+gustadevrj@users.noreply.github.com>
2021-07-27 18:07:57 -0300

    Echo Adicionado!

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
-echo("<br><br>XXXXXXXXXX XXXXXXXXXX XXXXXXXXXX<br><br><br>");
+echo("<br><br>XXXXXXXXXX XXXXXXXXXX XXXXXXXXXX XXXXXXXXXX XXXXXXXXXX XXXXXXXXXX<br><br><br>");
+
+echo("<br><br>9999999999<br><br><br>");
 
 ?>
